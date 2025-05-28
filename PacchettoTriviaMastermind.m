5bdfa459e2ee198c43d483cf826cf05ec8445a01
Angelo Greco <angelo.greco2@studio.unibo.it>
2025-05-28 18:50:26 +0200

    push 2

--- PacchettoTriviaMastermind.m
+++ PacchettoTriviaMastermind.m
@@ -23,7 +23,7 @@ Begin["`Private`"];
 (*
   Spiegazione del funzionamento per 'triviaData':
   Questa definizione impiega una tecnica nota come "caricamento differito" (o "lazy loading") con "memoizzazione".
-  L'operatore ':=' fa s\[IGrave] che l'operazione specificata (in questo caso, caricaTriviaDaCSV)
+  L'operatore ':=' fa s\[IGrave] che l'operazione specificata (in questo caso, CaricaTriviaDaCSV)
   non venga eseguita immediatamente, ma solo la prima volta che si fa effettivamente uso di 'triviaData'.
 
   Durante questo primo utilizzo, l'istruzione interna 'triviaData = ...' esegue due compiti:
@@ -39,14 +39,14 @@ Begin["`Private`"];
   (probabilmente per problemi di scoping tra notebook e pacchetto)
   
   Calcolando triviaData = LoadQuestions...  qui sotto come variabile globale porta alla ricalcolazione della variabile decine di volte,
-  testato aggiungendo un Print alla funzione di caricaTriviaDaCSV
+  testato aggiungendo un Print alla funzione di CaricaTriviaDaCSV
   Calcolando triviaData := LoadQuestions... nello stesso modo porta lo stesso problema, entrambi i casi portano a 
   perdite di prestazione, anche con cache automatiche per il risultato
 
   erano presenti un paio di cose extra che pensavo fossero necessario per le variabili globali fatte cos\[IGrave] 
   ma aveva ragione che potevano essere tolte.
 *)
-triviaData := triviaData = caricaTriviaDaCSV["trivia.csv"];
+triviaData := triviaData = CaricaTriviaDaCSV["trivia.csv"];
 
 
 (* Lista dei colori usati da Mastermind *)
@@ -95,13 +95,8 @@ Tuttavia, il risultato visivo finale anche in questa schermata rimane ben propor
 La risoluzione dello schermo viene ottenuta tramite il FrontEnd.
 In caso di errore, usa un valore di default (1920x1080) *)
 aggiornaDimensioniSchermo[] := Module[{w, h, scale},
-<<<<<<< HEAD
 	Quiet@Check[
 		{w, h} = FrontEndExecute @ FrontEnd`Value[FE`getScreenSize[]],
-=======
-	  Quiet@Check[
-	    {w, h} = FrontEndExecute @ FrontEnd`Value[FE`getScreenSize[]],
->>>>>>> 8b437981bef1551cd03842b22a49f70e37deb246
 	    {w, h} = {1920, 1080}
 	  ];
 	  scale = Min[w, h]/15;
@@ -857,11 +852,7 @@ interfacciaGriglia[seed_, lunghezzaCombinazione_, numeroTentativi_, allowDuplica
                                     (* Viene generata la griglia di feedback colorati.
                                     Si avranno tanti pallini di feedback quanto \[EGrave] lunga la
                                     combinazione da indovinare per la partita iniziata. *)
-<<<<<<< HEAD
                                     Dynamic[caricaRigaFeedback[x, hintFeedbackHistory, lunghezzaCombinazione]],
-=======
-                                    Dynamic[decidiFeedbackRiga[x, hintFeedbackHistory, lunghezzaCombinazione]],
->>>>>>> 8b437981bef1551cd03842b22a49f70e37deb246
 
                                     Spacer[50],
                                     
@@ -903,7 +894,6 @@ interfacciaGriglia[seed_, lunghezzaCombinazione_, numeroTentativi_, allowDuplica
 			                                       per facilitarne la lettura, ma senza successo *)
 			                                    Function[
 				                                    If[partitaInCorso,
-<<<<<<< HEAD
 														(* Garantisce che correct[[x]] esista *)
 														If[Length[correct] < x, AppendTo[correct, {}]];
 														
@@ -968,77 +958,6 @@ interfacciaGriglia[seed_, lunghezzaCombinazione_, numeroTentativi_, allowDuplica
 														tentativoList = ConstantArray[None, lunghezzaCombinazione];
 													]
 												]
-=======
-					                                    (* Prima di processare il tentativo, assicuriamoci che esista una entry per l'eventuale aiuto in questa riga. *)
-					                                    (* Questo serve per mantenere l'allineamento della griglia degli aiuti, anche se l'aiuto non viene usato. *)
-					                                    If[Length[correct] < x,
-					                                        (* Se la lunghezza della lista correct \[EGrave] minore di x, viene aggiunto un elemento vuoto *)
-					                                        AppendTo[correct, {}];
-					                                    ];
-					                                    (* Viene valutato il tentativo attuale *)
-					                                    valutazioneTentativo=valutaTentativo[soluzioneList, tentativoList, numeroTentativi, turn];
-					                                        (* Il risultato della valutazione (feedback) viene salvato nella variabile hintFeedbackHistory *)
-					                                    hintFeedbackHistory[[turn]]=Table[{tentativoList[[i]], valutazioneTentativo[[2]][[i]]}, {i, Length[tentativoList]}];
-					                                    
-					                                    If[valutazioneTentativo[[1]] === mastermindProsegui, turn++];
-					                                    
-					                                    (* Se il feedback indica vittoria: si ferma la partita, si mostra un dialog
-					                                    di vittoria e tutte le variabili principali vengono resettate per preparare
-					                                    una nuova partita *)
-					                                    If[Length[valutazioneTentativo] > 0 && valutazioneTentativo[[1]] === mastermindVittoria,
-					                                    
-						                                    partitaInCorso=False;
-						                                    
-						                                    mostraDialogFinePartita[True,
-							                                    Function[{},
-								                                    (* Reset delle variabili *)
-								                                    gridItemsColors=Table[Opacity[0.2, Black], {numeroTentativi}, {lunghezzaCombinazione}];
-								                                    hintFeedbackHistory=ConstantArray[{}, numeroTentativi];
-								                                    turn=1;
-								                                    colorsList=paletteColori;
-								                                    selectedItem={1, 1};
-								                                    soluzioneList=generaCodiceSegreto[seed, lunghezzaCombinazione, allowDuplicates];
-								                                    tentativoList=ConstantArray[None, lunghezzaCombinazione];
-								                                    valutazioneTentativo={};
-								                                    partitaInCorso=True;
-								                                    questionCounter=0;
-								                                    correct={};
-							                                    ],
-							                                    setScreen
-						                                    ];
-					                                    ];
-					                                    
-                                                        (* Se il feedback indica sconfitta: si ferma la partita, si mostra il dialog
-                                                        di sconfitta e, anche qui, tutto viene resettato *)
-					                                    If[Length[valutazioneTentativo] > 0 && valutazioneTentativo[[1]] === mastermindSconfitta,
-						                                    partitaInCorso=False;
-						                                    
-						                                    mostraDialogFinePartita[False,
-							                                    Function[{},
-								                                    (* Reset delle variabili *)
-								                                    gridItemsColors=Table[Opacity[0.2, Black], {numeroTentativi}, {lunghezzaCombinazione}];
-								                                    hintFeedbackHistory=ConstantArray[{}, numeroTentativi];
-								                                    turn=1;
-								                                    colorsList=paletteColori;
-								                                    selectedItem={1, 1};
-								                                    soluzioneList=generaCodiceSegreto[seed, lunghezzaCombinazione, allowDuplicates];
-								                                    tentativoList=ConstantArray[None, lunghezzaCombinazione];
-								                                    valutazioneTentativo={};
-								                                    partitaInCorso=True;
-								                                    questionCounter=0;
-								                                    correct={};
-							                                    ],
-							                                    setScreen
-						                                    ];
-					                                    ];
-					                                    
-					                                    (* Al termine, il turn viene incrementato (se la partita continua)
-					                                    e tentativoList azzerato per la nuova riga *)
-					                                    selectedItem={turn, 1};
-					                                    tentativoList=ConstantArray[None, lunghezzaCombinazione];
-				                                    ]
-			                                    ]   
->>>>>>> 8b437981bef1551cd03842b22a49f70e37deb246
 		                                    ],
 		                                    
 		                                    (* Caso 2: turno corrente ma tentativo incompleto.
@@ -1084,15 +1003,11 @@ interfacciaGriglia[seed_, lunghezzaCombinazione_, numeroTentativi_, allowDuplica
 									        correct,         
 									        x,               (* La 'x' corrente dalla Table (riga) *)
 									        turn,            
-<<<<<<< HEAD
 									        emptyResultPlaceholder,   
-=======
-									        emptyResultPlaceholder, 
->>>>>>> 8b437981bef1551cd03842b22a49f70e37deb246
 									        partitaInCorso,  
 									        Function[        (* Questa \[EGrave] la funzione di callback per il ClickPane *)
 									            If[partitaInCorso,
-									                AppendTo[correct, mostraDomandeTrivia[seed + questionCounter, decidiSuggerimentoLogica[hintFeedbackHistory, soluzioneList]]];
+									                AppendTo[correct, MostraDomandeTrivia[seed + questionCounter, DecidiSuggerimentoLogica[hintFeedbackHistory, soluzioneList]]];
 									                questionCounter++;
 									            ]
 									        ]
@@ -1120,12 +1035,8 @@ interfacciaGriglia[seed_, lunghezzaCombinazione_, numeroTentativi_, allowDuplica
 	] (* Fine Framed *)
 ]
 
-<<<<<<< HEAD
 (* === Funzione di rendering per i dischi di feedback === *)
 caricaRigaFeedback[x_, hintFeedbackHistory_, lunghezzaCombinazione_] := Module[
-=======
-decidiFeedbackRiga[x_, hintFeedbackHistory_, lunghezzaCombinazione_] := Module[
->>>>>>> 8b437981bef1551cd03842b22a49f70e37deb246
 	{feedbackSymbols, feedbackColors},
 	
 	(* Estrae i simboli di feedback dalla cronologia per la riga x.
@@ -1238,7 +1149,7 @@ In  aggiunta, si hanno opzioni per riavviare il gioco, tornare al menu e chiuder
 Parametri:
 - hasWon: True se il giocatore ha vinto, False altrimenti
 - onRestartFunc: funzione opzionale da eseguire se si sceglie Restart *)
-mostraDialogFinePartita[hasWon_, onRestartFunc_: Null, setScreen_] :=
+MostraDialogFinePartita[hasWon_, onRestartFunc_: Null, setScreen_] :=
     CreateDialog[
         DynamicModule[{}, (* Manteniamo questo DynamicModule per il contesto del Dialog *)
             Pane[
@@ -1319,12 +1230,12 @@ mostraDialogFinePartita[hasWon_, onRestartFunc_: Null, setScreen_] :=
 
 (* Mostra una finestra di dialogo (dialog) con una domanda trivia e opzioni a scelta multipla. *)
 (* Il dialogo \[EGrave] modale, quindi la funzione sospende l'esecuzione finch\[EAcute] il dialogo non viene chiuso. *)
-(* @param seed: Numero intero usato per selezionare e mescolare la domanda tramite preparaTriviaData. *)
+(* @param seed: Numero intero usato per selezionare e mescolare la domanda tramite PreparaTriviaData. *)
 (* @param hintToGive: La struttura dell'indizio (solitamente {colore, posizione_o_codice_mancante}) che verr\[AGrave] mostrata se la risposta \[EGrave] corretta. Questa stessa struttura sar\[AGrave] il valore restituito dalla funzione in caso di risposta corretta. *)
 (* @return: La struttura 'hintToGive' se \[EGrave] stata selezionata la risposta corretta. *)
 (* Restituisce Missing["WrongAnswer"] se \[EGrave] stata selezionata una risposta errata o se il dialogo \[EGrave] stato chiuso prematuramente (es. tramite il pulsante di chiusura della finestra del sistema operativo). *)
 (* Restituisce $Failed se il dialogo \[EGrave] stato chiuso prima di qualsiasi interazione e il risultato non \[EGrave] stato impostato esplicitamente (raro, data la gestione con NotebookEventActions). *)
-mostraDomandeTrivia[seed_Integer, hintToGive_] := Module[
+MostraDomandeTrivia[seed_Integer, hintToGive_] := Module[
     {
         questionWindow,
         result = $Failed,
@@ -1357,10 +1268,10 @@ mostraDomandeTrivia[seed_Integer, hintToGive_] := Module[
     , HoldAll]; 
 
     (* Prepara i dati della domanda *)
-    {initialQuestionData, initialOptionsData, initialCorrectIndexData} = preparaTriviaData[seed];
+    {initialQuestionData, initialOptionsData, initialCorrectIndexData} = PreparaTriviaData[seed];
 
     questionWindow = CreateDialog[
-        creaDialogTriviaUI[
+        CreaDialogTriviaUI[
             initialQuestionData,
             initialOptionsData,
             initialCorrectIndexData,
@@ -1388,7 +1299,7 @@ mostraDomandeTrivia[seed_Integer, hintToGive_] := Module[
 ];
 
 
-creaDialogTriviaUI[
+CreaDialogTriviaUI[
     initialQuestionData_, 
     initialOptionsData_, 
     initialCorrectIndexData_,
@@ -1407,7 +1318,7 @@ creaDialogTriviaUI[
     Dynamic@Refresh[
         Switch[displayStateInternal,
             "question",
-            domandeTriviaUI[
+            DomandeTriviaUI[
                 localQuestion,
                 localOptions,
                 localCorrectIndex,
@@ -1419,10 +1330,10 @@ creaDialogTriviaUI[
             ],
             
             "correct_show_hint",
-            triviaCorrettoUI[hintToGiveOuter, performCloseCallback],
+            TriviaCorrettoUI[hintToGiveOuter, performCloseCallback],
             
             "incorrect_show_message",
-            triviaIncorrettoUI[localOptions, localCorrectIndex, performCloseCallback],
+            TriviaIncorrettoUI[localOptions, localCorrectIndex, performCloseCallback],
             
             _, 
             Style["Error: Dialog content issue.", Red, Bold]
@@ -1432,7 +1343,7 @@ creaDialogTriviaUI[
 ]
 
 
-domandeTriviaUI[
+DomandeTriviaUI[
     questionData_, (* contiene questionData["Question"] *)
     optionsList_, 
     correctOptionIndex_, 
@@ -1484,7 +1395,7 @@ Column[
 ]
 
 
-triviaCorrettoUI[hintToGiveLocal_List, closeActionCallback_] :=
+TriviaCorrettoUI[hintToGiveLocal_List, closeActionCallback_] :=
     With[{theCol = hintToGiveLocal[[1]], posVal = hintToGiveLocal[[2]]}, 
         Column[
             {
@@ -1530,7 +1441,7 @@ triviaCorrettoUI[hintToGiveLocal_List, closeActionCallback_] :=
     ];
 
 
-triviaIncorrettoUI[optionsList_List, correctOptionIndex_Integer, closeActionCallback_] :=
+TriviaIncorrettoUI[optionsList_List, correctOptionIndex_Integer, closeActionCallback_] :=
     Column[
         {
         (* Titolo "Incorrect!" *)
@@ -1571,7 +1482,7 @@ Valore di ritorno:
 - Un Dataset Mathematica in cui ogni riga \[EGrave] un'associazione (nome_colonna -> valore_dato),
 oppure $Failed se si verifica un errore durante il caricamento del file o
 l'interpretazione del suo contenuto come dati CSV *)
-caricaTriviaDaCSV[path_String] := Module[
+CaricaTriviaDaCSV[path_String] := Module[
 	{csvText, data, headers, rows, dataset},
 	
 	(* Fase 1: Importa il contenuto grezzo del file CSV come testo. *)
@@ -1600,7 +1511,7 @@ caricaTriviaDaCSV[path_String] := Module[
 (* Prepara i dati di una domanda: seleziona la domanda in base al seed, estrae le opzioni di risposta e l'indice della risposta corretta.
 @param seed: Intero usato per la selezione (pseudo)casuale della domanda.
 @return: Lista contenente {domandaSelezionata, opzioniDisponibili, indiceRispostaCorretta} *)
-preparaTriviaData[seed_Integer] := Module[
+PreparaTriviaData[seed_Integer] := Module[
 	{questionIndex, options, rawCorrectIndex, selectedQuestion, optionKeys, correctIndex},
 	  
 	SeedRandom[seed];
@@ -1635,7 +1546,7 @@ Priorit\[AGrave] degli Indizi (la funzione restituisce il primo indizio applicab
 - {colore, Missing["PositionNotApplicable"]}: Indizio senza posizione specifica.
 - {colore, posizione_Integer}: Indizio con posizione.
 - {colore_placeholder, Missing["NoSimpleHintAvailable"]}: Indizio di fallback. *)
-decidiSuggerimentoLogica[hintFeedbackHistoryInput_List, soluzioneListInput_List] := Catch[
+DecidiSuggerimentoLogica[hintFeedbackHistoryInput_List, soluzioneListInput_List] := Catch[
     Module[
         {
             soluzione = soluzioneListInput,
@@ -1653,13 +1564,13 @@ decidiSuggerimentoLogica[hintFeedbackHistoryInput_List, soluzioneListInput_List]
         tentativiEffettuati = Select[hintFeedbackHistory, # =!= {} &];
 
         (* --- Priorit\[AGrave] 1: Gestione del caso "Nessun Tentativo Giocato" --- *)
-        gestisciHintNessunTentativo[soluzione, tentativiEffettuati]; (* Pu\[OGrave] fare Throw *)
+        GestisciHintNessunTentativo[soluzione, tentativiEffettuati]; (* Pu\[OGrave] fare Throw *)
 
         (* --- Priorit\[AGrave] 2: Cerca colori della soluzione non ancora "confermati" --- *)
-        gestisciHintColoreNonConfermato[soluzione, tentativiEffettuati, feedbackEsatto, feedbackParziale]; (* Pu\[OGrave] fare Throw *)
+        GestisciHintColoreNonConfermato[soluzione, tentativiEffettuati, feedbackEsatto, feedbackParziale]; (* Pu\[OGrave] fare Throw *)
         
         (* --- Priorit\[AGrave] 3: Indizio per un colore con 'feedbackParziale' ma mai 'feedbackEsatto' --- *)
-        gestisciHintConPosizione[soluzione, tentativiEffettuati, n, feedbackEsatto, feedbackParziale]; (* Pu\[OGrave] fare Throw *)
+        GestisciHintConPosizione[soluzione, tentativiEffettuati, n, feedbackEsatto, feedbackParziale]; (* Pu\[OGrave] fare Throw *)
 
         (* --- Priorit\[AGrave] 4: Indizio di Fallback --- *)
         (* Se nessun altro indizio specifico \[EGrave] stato generato, fornisce un indizio generico. *)
@@ -1674,7 +1585,7 @@ Se non ci sono tentativi, lancia un indizio con un colore casuale dalla soluzion
 @param soluzione_List: La lista dei colori della soluzione segreta.
 @param tentativiEffettuati_List: La lista dei tentativi che contengono dati (non vuoti).
 @effect: Pu\[OGrave] lanciare (Throw) un indizio se non ci sono tentativi. *)
-gestisciHintNessunTentativo[soluzione_List, tentativiEffettuati_List] :=
+GestisciHintNessunTentativo[soluzione_List, tentativiEffettuati_List] :=
     If[Length[tentativiEffettuati] == 0,
         Throw[{RandomChoice[soluzione], Missing["PositionNotApplicable"]}]
     ];
@@ -1688,7 +1599,7 @@ e, se ne trova uno, lancia un indizio con quel colore.
 @param feedbackEsattoSimbolo_: Il simbolo che rappresenta un feedback esatto.
 @param feedbackParzialeSimbolo_: Il simbolo che rappresenta un feedback parziale.
 @effect: Pu\[OGrave] lanciare (Throw) un indizio se trova un colore non confermato. *)
-gestisciHintColoreNonConfermato[soluzione_List, tentativiEffettuati_List, feedbackEsattoSimbolo_, feedbackParzialeSimbolo_] :=
+GestisciHintColoreNonConfermato[soluzione_List, tentativiEffettuati_List, feedbackEsattoSimbolo_, feedbackParzialeSimbolo_] :=
     Module[
         {
             coloriUniciSoluzione, 
@@ -1737,7 +1648,7 @@ gestisciHintColoreNonConfermato[soluzione_List, tentativiEffettuati_List, feedba
 @param feedbackParzialeSimbolo_: Il simbolo che rappresenta un feedback parziale.
 @effect: Pu\[OGrave] lanciare (Throw) un indizio {colore, posizione} se trova un candidato valido. *)
 
-gestisciHintConPosizione[
+GestisciHintConPosizione[
     soluzione_List, 
     tentativiEffettuati_List, 
     nPioli_Integer, 
